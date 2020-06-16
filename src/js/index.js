5603b8c0efcbd1e2ef130129862195f7162bd4b4
Magdalena Lisicka <magdalisicka@yahoo.co.uk>
2020-06-16 23:24:35 +0100

    files trafnsfer completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../scss/main.scss';
+import moment from 'moment';
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -7,3 +8,31 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+
+
+const button = document.querySelector('.action--js');
+
+console.log(button)
+
+/*button.addEventListener('click', () => {
+    const heading = document.querySelector(".main__heading--js");
+    heading.innerHTML = `Witaj Drogi Odwiedzajacy, nazaywam sie Magda`;
+    heading.classList.toggle('klasa-z-js');
+});*/
+
+
+
+const hamburger = document.querySelector('.hamburger--js');
+
+
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--open');
+})
+
+const startOfDay = moment().startOf('day').fromNow();
+
+const timePlaceholder = document.querySelector('.time--js');
+
+timePlaceholder.innerHTML = startOfDay;
